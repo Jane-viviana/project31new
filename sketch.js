813a0f9f19a7bd4b3e564de41cb53d7bab40df91
Jane <73689728+Jane-viviana@users.noreply.github.com>
2021-02-08 19:16:37 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -18,13 +18,7 @@ function setup() {
 
   ground = new Ground(350,750,800,15);
 
-  //var render = Matter.Render.create({element:document.body,engine:engine,options:{width:600,height:800}})
- //Matter.Render.run(render);
-}
-
-function draw() {
-  Engine.update(engine);
-  background(0);
+  //division and plinkos
 
   for(var k = 0; k <=width; k = k+100){
     division.push(new Wall(k,600,15,300));
@@ -41,12 +35,22 @@ function draw() {
   for(var a = 0; a <= width; a = a+100){
     plinko.push(new Plinko(a,400,15,15));
   }
-  for(var b = 0; b <= width; b = b+25){
+
+  //var render = Matter.Render.create({element:document.body,engine:engine,options:{width:600,height:800}})
+ //Matter.Render.run(render);
+}
+
+function draw() {
+  Engine.update(engine);
+  background(0);
+
+  
+  /*for(var b = 0; b <= width; b = b+25){
     particle.push(new Particle(b,0,10,10));
-  }
+  }*/
 
   if(frameCount % 60 === 0){
-    var rand = Math.Round(random(20,100));
+    var rand = Math.round(random(20,700));
     particle.push(new Particle(rand,0,10,10));
    }
 
